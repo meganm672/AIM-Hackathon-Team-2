fd42d41b76b9321c4ccecc9fada08eeb96bacd99
meganm672 <meganm672@gmail.com>
2024-05-08 21:09:07 -0400

    dashboard started

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,10 @@
+import Dashboard from "./components/Dashboard";
+
 const App = () => {
 
     return (
       <div className="App">
+        <Dashboard />
         Hello world!
       </div>
     );
