9a2405e250c547223a9e7cc1d92a2b8ccc74b60c
Nat-crit20 <greuelnathan74@gmail.com>
2024-05-15 11:39:07 -0500

    Fixing merging issue

--- client/components/DashboardComponents/GoalsTable.jsx
+++ client/components/DashboardComponents/GoalsTable.jsx
@@ -11,15 +11,15 @@ import Button from "@mui/material/Button";
 import LinearProgressWithLabel from "./Utils";
 import { PiPencilSimpleThin } from "react-icons/pi";
 import { BiTransfer } from "react-icons/bi";
-import Checkbox from '@mui/material/Checkbox';
-import Chip from '@mui/material/Chip'
-import Stack from '@mui/material/Stack'
-import IconButton from '@mui/material/IconButton';
+import Checkbox from "@mui/material/Checkbox";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
 const priorityColorMap = {
   low: { backgroundColor: "#E4FDEB", color: "#48C76A" }, // Green
   medium: { backgroundColor: "#FFF3B4", color: "#E7B147" }, // Yellow
   high: { backgroundColor: "#F9E7D9", color: "#E78A47" }, // Orange
-  critical: { backgroundColor: "#FFEBEB", color: "#EB5757" } // Red
+  critical: { backgroundColor: "#FFEBEB", color: "#EB5757" }, // Red
 };
 
 const chipStyle = {
@@ -32,7 +32,7 @@ const buttonStyle = {
   height: "40px",
   backgroundColor: "#F1F3F4", // Gray color
   color: "#706F6F",
-  fontSize: 15
+  fontSize: 15,
 };
 
 const GoalsTable = ({ goalData, category }) => {
@@ -47,7 +47,6 @@ const GoalsTable = ({ goalData, category }) => {
       textTransform: "capitalize",
     };
   };
-
   return (
     <div>
       <TableContainer component={Paper}>
@@ -82,7 +81,7 @@ const GoalsTable = ({ goalData, category }) => {
                   />
                 </TableCell>
                 <TableCell align="right">{row.deadline}</TableCell>
-                <TableCell align="right" >
+                <TableCell align="right">
                   <Chip
                     label={row.priority}
                     style={{
@@ -97,13 +96,10 @@ const GoalsTable = ({ goalData, category }) => {
                     <IconButton key={index} style={{ padding: 0 }}>
                       {badge.icon}
                     </IconButton>
-                    
                   ))}
-
                 </TableCell>
                 <TableCell align="right">
                   <Stack direction={"row"}>
-
                     <IconButton style={buttonStyle}>
                       <PiPencilSimpleThin />
                     </IconButton>
