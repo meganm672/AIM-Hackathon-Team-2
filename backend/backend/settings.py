cee52d5477702410c52c55064a3be526cf2f83c1
Nat-crit20 <greuelnathan74@gmail.com>
2024-05-16 19:48:09 -0500

    Updated settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["aim-hackathon-team-2.onrender.com"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5173",  # Replace with your frontend application's origin
+    "http://127.0.0.1:5173", 
+    "http://localhost:5173" # Replace with your frontend application's origin
 ]
 
 # Application definition
