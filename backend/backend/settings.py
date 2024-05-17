d1d067bdebb6e25aad2a388d0658b976bc639e1c
Mudra Patel <76879120+mudrap17@users.noreply.github.com>
2024-05-17 13:00:06 -0400

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = ["aim-hackathon-team-2.onrender.com"]
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173", 
     "http://localhost:5173",
-    "https://saveup-qfes.onrender.com" # Replace with your frontend application's origin
+    "https://saveup-qfes.onrender.com",
+    "https://saveup-wbae.onrender.com"# Replace with your frontend application's origin
 ]
 
 # Application definition
